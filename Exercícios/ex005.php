c1cc2f8f3ffdd74bb61206ea2731d86e70045532
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-04-29 20:43:29 -0300

    ex005

--- Exercícios/ex005.php
+++ Exercícios/ex005.php
@@ -2,6 +2,6 @@
     # 05 - Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor. 
     $valor = 90;
     $antecessor = $valor - 1;
-    echo "Valor: $valor" . '<br/>';
+    echo "Valor: $valor";
     echo "Antecessor: $antecessor";
 ?>
